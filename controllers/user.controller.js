e9a2e109c5f3ad31dd4782f86e023f1e86f49271
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-03-31 11:27:52 +0200

    delete follow unfollow requests

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,6 +9,8 @@ module.exports.getAllUsers = async (req, res) => {
 }
 
 
+
+
 // un seul utilisateur
 module.exports.userInfo = (req, res) => {
     // console.log(req.params); les params dans l'url
@@ -20,6 +22,9 @@ module.exports.userInfo = (req, res) => {
         else console.log('ID unknown : ' + err);
     }).select('-password');
 };
+
+
+
 
 module.exports.updateUser = async (req,res) => {
     if(!ObjectID.isValid(req.params.id))
@@ -42,4 +47,84 @@ module.exports.updateUser = async (req,res) => {
     } catch (err) {
         if(err) return res.json(500).senc({ message: err});
     }
+};
+
+
+
+
+module.exports.deleteUser = async (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+    
+    try {
+        await UserModel.remove({_id: req.params.id}).exec();
+        res.status(200).json({message: "Successfully deleted. "})
+    } catch (err) {
+        return res.status(500).json({ message: err});
+    }
+};
+
+
+
+
+
+module.exports.follow = async (req,res) => {
+    if(!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+    try {
+        // add to the follower list
+        await UserModel.findByIdAndUpdate(
+            req.params.id, //ID de la personne qui veut suivre
+            {$addToSet: {following: req.body.idToFollow}}, // ID de la personne qui est suivie
+            {new: true, upsert: true},
+            (err, docs) => {
+                if (!err) res.status(201).json(docs);
+                else return res.status(400).json(err);
+            }
+        );
+        // add to following list
+        await UserModel.findByIdAndUpdate(
+        req.body.idToFollow,
+        {$addToSet: {followers: req.params.id}},
+        {new: true, upsert: true},
+            (err, docs) => {
+                //if (!err) res.status(201).json(docs);
+                if (err) return res.status(400).json(err);
+            }
+        )
+    } catch (err) {
+        return res.status(500).json({ message: err});
+    }
+};
+
+
+module.exports.unfollow = async (req,res) => {
+    if(!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow))
+        return res.status(400).send('ID unknown : ' + req.params.id)
+
+        try {
+            // add to the follower list
+            await UserModel.findByIdAndUpdate(
+                req.params.id, //ID de la personne qui veut suivre
+                {$pull: {following: req.body.idToUnfollow}}, // ID de la personne qui est suivie
+                {new: true, upsert: true},
+                (err, docs) => {
+                    if (!err) res.status(201).json(docs);
+                    else return res.status(400).json(err);
+                }
+            );
+            // add to following list
+            await UserModel.findByIdAndUpdate(
+            req.body.idToUnfollow,
+            {$pull: {followers: req.params.id}},
+            {new: true, upsert: true},
+                (err, docs) => {
+                    //if (!err) res.status(201).json(docs);
+                    if (err) return res.status(400).json(err);
+                }
+            )
+        } catch (err) {
+            return res.status(500).json({ message: err});
+        }
 }
