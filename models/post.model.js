a922088295003d3a075a1c2f80752998c17996ed
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-01 11:07:35 +0200

    bdd pour les posts

---
+++ models/post.model.js
@@ -0,0 +1,40 @@
+const mongoose = require ('mongoose');
+const PostSchema = new mongoose.Schema(
+    {
+        posterId: {
+            type: String,
+            require: true
+        },
+        message: {
+            type: String,
+            trim: true,
+            maxLength: 500,
+        },
+        picture: {
+            type: String,
+        },
+        video: {
+            type: String,
+        },
+        likers: {
+            type: [String], //id des utilisateurs qui auront liké le post
+            required: true,
+        },
+        comments: {
+            type: [
+                {
+                    commenterId: string,
+                    commenterPseudo: String,
+                    text: String,
+                    timestamp: Number,
+                }
+            ],
+            required: true,
+        }
+    },
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('post', PostSchema)
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const {checkUser, requireAuth} = require('./middleware/auth.middleware');
@@ -21,6 +22,7 @@ app.get('/jwtid', requireAuth, (req,res) => {
 
 // routes 
 app.use('/api/user', userRoutes);
+app.use('/api/post', postRoutes);
 
 
 // server
