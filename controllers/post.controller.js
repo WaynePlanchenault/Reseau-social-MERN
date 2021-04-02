992295975fd667ede636714e20dfbe7f7b406a95
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-02 18:55:12 +0200

    delete crud commentaires

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -179,4 +179,24 @@ module.exports.editCommentPost = (req, res) => {
 module.exports.deleteCommentPost = (req, res) => {
   if (!ObjectID.isValid(req.params.id))
     return res.status(400).send("ID unknown : " + req.params.id);
+
+  try {
+    return PostModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $pull: {
+          comments: {
+            _id: req.body.commentId,
+          },
+        },
+      },
+      { new: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(400).send(err);
+      }
+    );
+  } catch (err) {
+    return res.status(400).send(err);
+  }
 };
