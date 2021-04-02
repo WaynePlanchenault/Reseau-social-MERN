391e0709552ac71d58faad2bc55f09f6de284db6
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-02 22:40:03 +0200

    upload image de profil

--- controllers/upload.controller.js
+++ controllers/upload.controller.js
@@ -33,4 +33,18 @@ module.exports.uploadProfil = async (req, res) => {
       `${__dirname}/../client/public/uploads/profil/${fileName}` // dans ce chemin il va créer un fichier
     )
   );
+
+  try {
+    await UserModel.findByIdAndUpdate(
+      req.body.userId,
+      { $set: { picture: "./uploads/profil/" + fileName } },
+      { new: true, upsert: true, setDefaultsOnInsert: true },
+      (err, docs) => {
+        if (!err) return res.send(docs);
+        else return res.status(500).send({ message: err });
+      }
+    );
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
 };
