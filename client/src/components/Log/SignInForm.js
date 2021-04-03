07e77d78d909897bdf818653a63618841fc7fb50
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-03 16:04:17 +0200

    champs controles formulaire signup

--- client/src/components/Log/SignInForm.js
+++ client/src/components/Log/SignInForm.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignInForm = () => {
-  return <div>connecter</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {};
+
+  return (
+    <form action="" onSubmit={handleLogin} id="sign-up-form">
+      <label htmlFor="email">Email</label>
+      <br />
+      <input
+        type="text"
+        name="email"
+        id="email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      />
+      <div className="email error"></div>
+      <br />
+      <label htmlFor="password">Mot de passe</label>
+      <br />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div className="password error"></div>
+      <br />
+      <input type="submit" value="Se connecter" />
+    </form>
+  );
 };
 
 export default SignInForm;
