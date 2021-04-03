395b161870389629b07f5ac04236693f5bb95560
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-03 15:12:27 +0200

    signin et signup boutons formulaire

---
+++ client/src/components/Log/SignUpForm.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUpForm = () => {
+  return <div>s'inscrire</div>;
+};
+
+export default SignUpForm;
---
+++ client/src/components/Log/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
+
+const Log = (props) => {
+  const [signUpModal, setSignUpModal] = useState(props.singup);
+  const [signInModal, setSignInModal] = useState(props.signin);
+
+  const handleModals = (e) => {
+    if (e.target.id === "register") {
+      setSignInModal(false);
+      setSignUpModal(true);
+    } else if (e.target.id === "login") {
+      setSignUpModal(false);
+      setSignInModal(true);
+    }
+  };
+
+  return (
+    <div>
+      <div className="connection-form">
+        <div className="form-container">
+          <ul>
+            <li
+              onClick={handleModals}
+              id="register"
+              className={signUpModal ? "active-btn" : null}
+            >
+              S'inscrire
+            </li>
+            {/* on demande si c'est true, si oui on applique la classe active-btn sinon rien*/}
+            <li
+              onClick={handleModals}
+              id="login"
+              className={signInModal ? "active-btn" : null}
+            >
+              Se connecter
+            </li>
+          </ul>
+          {signUpModal && <SignUpForm />}
+          {signInModal && <SignInForm />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Log;
