bfbeaa619bf300343906d31a4a908d585b33fc2d
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-03 17:20:58 +0200

    axios et signIn

--- client/src/components/Log/SignInForm.js
+++ client/src/components/Log/SignInForm.js
@@ -5,7 +5,33 @@ const SignInForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {};
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const emailError = document.querySelector(".email.error");
+    const passwordError = document.querySelector(".password.error");
+
+    axios({
+      method: "post",
+      url: `${process.env.REACT_APP_API_URL}api/user/login`,
+      withCredentials: true,
+      data: {
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        if (res.data.errors) {
+          emailError.innerHTML = res.data.errors.email;
+          passwordError.innerHTML = res.data.errors.password;
+          // envoie les erreurs codés en back précédemment
+        } else {
+          window.location = "/";
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <form action="" onSubmit={handleLogin} id="sign-up-form">
