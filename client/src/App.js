7d2ca44363bcac7aa85f3ea9c33b9f46d2118dab
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-04 16:04:05 +0200

    id user dans le state a la racine plus redirection update profil

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
+import axios from "axios";
 
 const App = () => {
+  const [uid, setUid] = useState(null);
+
+  // permet de stocker l'id dans le state à la racine de l'application
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}jwtid`,
+        withCredentials: true,
+      })
+        .then((res) => setUid(res.data))
+        .catch((err) => console.log("No token"));
+    };
+    fetchToken();
+  }, []); // [] évite la requête à l'infini
+
   return (
-    <div>
+    <UidContext.Provider value={uid}>
       <Routes />
-    </div>
+    </UidContext.Provider>
   );
 };
 
