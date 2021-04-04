bd34d12217574ce3b59439f339c0c8fe488ff953
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-04 16:32:36 +0200

    formulaire inscription state et erreurs cote client

--- client/src/components/Log/SignUpForm.js
+++ client/src/components/Log/SignUpForm.js
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const SignUpForm = () => {
-  return <div>s'inscrire</div>;
+  const [pseudo, setPseudo] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [controlPassword, setControlPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    // on selection nos ID que l'on met dans une constante qu'on utilisera pour incrémenter nos erreurs en html côté client
+    const terms = document.getElementById("terms");
+    const pseudoError = document.querySelector(".pseudo.error");
+    const emailError = document.querySelector(".email.error");
+    const passwordError = document.querySelector(".password.error");
+    const passwordConfirmError = document.querySelector(
+      ".password-confirm.error"
+    );
+    const termsError = document.querySelector(".terms.error");
+
+    // à chaque fois qu'on relance le formulaire on lui injecte une string vide pour supprimer le message d'erreur
+    passwordConfirmError.innerHTML = "";
+    termsError.innerHTML = "";
+
+    // le premier if permet de tester la condition sur les deux if et ne pas s'arrêter au premier positif et donc si besoin afficher les deux
+    if (password !== controlPassword || !terms.checked) {
+      if (password !== controlPassword)
+        passwordConfirmError.innerHTML =
+          "les mots de passe ne correspondent pas";
+
+      if (!terms.checked)
+        termsError.innerHTML = "veuillez valider les conditions générales";
+    }
+  };
+
+  return (
+    <form action="" onSubmit={handleRegister} id="sign-up-form">
+      <label htmlFor="pseudo">Pseudo</label>
+      <br />
+      <input
+        type="text"
+        name="pseudo"
+        id="pseudo"
+        onChange={(e) => setPseudo(e.target.value)}
+      />
+      <div className="pseudo error"></div>
+      <br />
+      <label htmlFor="email">Email</label>
+      <br />
+      <input
+        type="text"
+        name="email"
+        id="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <div className="email error"></div>
+      <br />
+      <label htmlFor="password">Mot de passe</label>
+      <br />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div className="password error"></div>
+      <br />
+      <label htmlFor="password-conf">Confirmer le mot de passe</label>
+      <input
+        type="password"
+        name="password"
+        id="password-conf"
+        onChange={(e) => setControlPassword(e.target.value)}
+      />
+      <div className="password-confirm error"></div>
+      <br />
+      <input type="checkbox" id="terms" />
+      <label htmlFor="terms">
+        J'accepte les{" "}
+        <a href="/" target="_blank" rel="noopener noreferrer">
+          Conditions générales
+        </a>
+      </label>
+      <div className="terms error"></div>;
+      <br />
+      <input type="submit" value="Valider inscription" />
+    </form>
+  );
 };
 
 export default SignUpForm;
