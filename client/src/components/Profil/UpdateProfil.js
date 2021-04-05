6782cd1d4e0056053494734c5860c59820712994
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-05 21:14:04 +0200

    following follower users reducer et action users au state partout

--- client/src/components/Profil/UpdateProfil.js
+++ client/src/components/Profil/UpdateProfil.js
@@ -10,6 +10,8 @@ const UpdateProfil = () => {
   const [updateForm, setUpdateForm] = useState(false);
   const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
+  const [followingPopup, setFollowingPopup] = useState(false);
+  const [followersPopup, setFollowersPopup] = useState(false);
 
   const handleUpdate = () => {
     dispatch(updateBio(userData._id, bio));
@@ -51,8 +53,29 @@ const UpdateProfil = () => {
             )}
           </div>
           <h4>Membre depuis le : {dateParser(userData.createdAt)}</h4>
+          <h5 onClick={() => setFollowingPopup(true)}>
+            Abonnements :{" "}
+            {
+              userData.following ? userData.following.length : "0"
+              /* si tu as des abonnements tu les affiches sinon tu affiches "0" */
+            }
+          </h5>
+          <h5 onClick={() => setFollowersPopup(true)}>
+            Abonnements : {userData.followers ? userData.followers.length : "0"}
+          </h5>
         </div>
       </div>
+      {followingPopup && (
+        <div className="popup-profil-container">
+          <div className="modal">
+            <h3>Abonnements</h3>
+            <span className="cross" onClick={() => setFollowingPopup(false)}>
+              &#10005;
+            </span>
+            <ul></ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
