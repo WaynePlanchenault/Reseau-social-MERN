15051c842b6731d1eb2c4bfcf6c69745ea63e0ee
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-05 12:52:13 +0200

    store en place

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import rootReducer from './reducers';
 
 const store = createStore (
   rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
