1df4cbe3baa69d68e4e230d5314bda33623288ce
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-05 12:33:07 +0200

    instalation redux et initialisation store

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/index.scss";
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore (
+  rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  <Provider store={store}>
+    <App />, document.getElementById("root"));
+  </Provider>
+  
