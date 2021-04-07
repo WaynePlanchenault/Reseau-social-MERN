273426c477dbe8fd92a340cda03fdcca0bcd7b4f
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-07 15:36:33 +0200

    affichage des commentaires photo nom et abonnement

--- client/src/components/Post/Card.js
+++ client/src/components/Post/Card.js
@@ -5,11 +5,13 @@ import FollowHandler from "../Profil/FollowHandler";
 import LikeButton from "./LikeButton";
 import { updatePost } from "../../actions/post.actions";
 import DeleteCard from "./DeleteCard";
+import CardComment from "./CardComment";
 
 const Card = ({ post }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isUpdated, setIsUpdated] = useState(false);
   const [textUpdate, setTextUpdate] = useState(null);
+  const [showComments, setShowComments] = useState(false);
   const usersData = useSelector((state) => state.usersReducer);
   const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
@@ -43,7 +45,7 @@ const Card = ({ post }) => {
                   })
                   .join("")
               }
-              alt=""
+              alt="poster-pic"
             />
           </div>
           <div className="card-right">
@@ -100,12 +102,17 @@ const Card = ({ post }) => {
             )}
             <div className="card-footer">
               <div className="comment-icon">
-                <img src="./img/icons/message1.svg" alt="comment" />
+                <img
+                  onClick={() => setShowComments(!showComments)}
+                  src="./img/icons/message1.svg"
+                  alt="comment"
+                />
                 <span>{post.comments.length}</span>
               </div>
               <LikeButton post={post} />
               <img src="./img/icons/share.svg" alt="" />
             </div>
+            {showComments && <CardComment post={post} />}
           </div>
         </>
       )}
---
+++ client/src/components/Post/CardComment.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import FollowHandler from "../Profil/FollowHandler";
+import { isEmpty } from "../Utils";
+
+const CardComment = ({ post }) => {
+  const [text, setText] = useState("");
+  const usersData = useSelector((state) => state.usersReducer);
+  const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleComment = () => {};
+
+  return (
+    <div className="comments-container">
+      {post.comments.map((comment) => {
+        return (
+          <div
+            className={
+              comment.commenterId === userData._id
+                ? "comment-container client"
+                : "comment-container"
+            }
+            key={comment._id}
+          >
+            <div className="left-part">
+              <img
+                src={
+                  !isEmpty(usersData[0]) &&
+                  usersData
+                    .map((user) => {
+                      if (user._id === comment.commenterId) return user.picture;
+                      else return null;
+                    })
+                    .join("")
+                }
+                alt="commenter-pic"
+              />
+            </div>
+            <div className="right-part">
+              <div className="comment-header">
+                <div className="pseudo">
+                  <h3>{comment.commenterPseudo}</h3>
+                  {comment.commenterId !== userData._id && (
+                    <FollowHandler
+                      idToFollow={comment.commenterId}
+                      type={"card"}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardComment;
