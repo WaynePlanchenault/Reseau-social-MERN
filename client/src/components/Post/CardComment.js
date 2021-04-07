5f93b127b03e03ef152360f207addd8ea03e8079
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-07 16:07:27 +0200

    laisser un commentaire

--- client/src/components/Post/CardComment.js
+++ client/src/components/Post/CardComment.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { addComment, getPosts } from "../../actions/post.actions";
 import FollowHandler from "../Profil/FollowHandler";
 import { isEmpty, timestampParser } from "../Utils";
 
@@ -9,7 +10,15 @@ const CardComment = ({ post }) => {
   const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
 
-  const handleComment = () => {};
+  const handleComment = (e) => {
+    e.preventDefault();
+
+    if (text) {
+      dispatch(addComment(post._id, userData._id, text, userData.pseudo))
+        .then(() => dispatch(getPosts()))
+        .then(() => setText(""));
+    }
+  };
 
   return (
     <div className="comments-container">
@@ -55,6 +64,19 @@ const CardComment = ({ post }) => {
           </div>
         );
       })}
+      {userData._id && (
+        <form action="" onSubmit={handleComment} className="comment-form">
+          <input
+            type="text"
+            name="text"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+            placeholder="Laisser un commentaire"
+          />
+          <br />
+          <input type="submit" value="Envoyer" />
+        </form>
+      )}
     </div>
   );
 };
