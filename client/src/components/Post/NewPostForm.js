760b345ab20bf4b601b9bcae335291ef116efb43
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-09 14:24:00 +0200

    rendu du formulaire nouvelle publication et annuler message

--- client/src/components/Post/NewPostForm.js
+++ client/src/components/Post/NewPostForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import { isEmpty } from "../Utils";
 
 const NewPostForm = () => {
@@ -9,6 +10,17 @@ const NewPostForm = () => {
   const [video, setVideo] = useState("");
   const [file, setFile] = useState();
   const userData = useSelector((state) => state.userReducer);
+
+  const handlePicture = () => {};
+
+  const handlePost = () => {};
+
+  const cancelPost = () => {
+    setMessage("");
+    setPostPicture("");
+    setVideo("");
+    setFile("");
+  };
 
   useEffect(() => {
     if (!isEmpty(userData)) setIsLoading(false);
@@ -23,8 +35,59 @@ const NewPostForm = () => {
           <div className="data">
             <p>
               <span>{userData.following ? userData.following.length : 0}</span>{" "}
-              Abonnements
+              Abonnement
+              {userData.following && userData.following.length > 1 ? "s" : null}
             </p>
+            <p>
+              <span>{userData.followers ? userData.followers.length : 0}</span>{" "}
+              Abonné
+              {userData.followers && userData.followers.length > 1 ? "s" : null}
+            </p>
+          </div>
+          <NavLink exact to="/profil">
+            <div className="user-info">
+              <img src={userData.picture} alt="user-img" />
+            </div>
+          </NavLink>
+          <div className="post-form">
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Quoi de neuf ?"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+
+            <div className="footer-form">
+              <div className="icon">
+                {isEmpty(video) && (
+                  <>
+                    <img src="./img/icons/picture.svg" alt="img" />
+                    <input
+                      type="file"
+                      id="file-upload"
+                      name="file"
+                      accept=".jpg, .jpeg, .png"
+                      onChange={(e) => handlePicture(e)}
+                    />
+                  </>
+                )}
+                {video && (
+                  <button onClick={() => setVideo("")}>Supprimer video</button>
+                )}
+              </div>
+              <div className="btn-send">
+                {message || postPicture || video.length > 20 ? (
+                  <button className="cancel" onClick={cancelPost}>
+                    Annuler message
+                  </button>
+                ) : null}
+
+                <button className="send" onClick={handlePost}>
+                  Envoyer
+                </button>
+              </div>
+            </div>
           </div>
         </>
       )}
