98e78ec9e0c39bede52582583ea18a88cfdbe042
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-09 18:26:17 +0200

    error reducer

---
+++ client/src/reducers/error.reducer.js
@@ -0,0 +1,15 @@
+import { GET_POST_ERRORS } from "../actions/post.actions";
+
+const initialState = { postErrors: [] };
+
+export default function errorReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_POST_ERRORS:
+      return {
+        postErrors: action.payload,
+        userErrors: [],
+      };
+    default:
+      return state;
+  }
+}
