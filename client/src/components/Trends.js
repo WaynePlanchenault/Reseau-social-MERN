65759b56873da2885dd4501bd3907e3e5939e038
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-10 19:38:44 +0200

    hooks du composant

--- client/src/components/Trends.js
+++ client/src/components/Trends.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Trends = () => {
+  const posts = useSelector((state) => state.allPostsReducer);
+  const usersData = useSelector((state) => state.usersReducer);
+  const dispatch = useDispatch();
+
   return <div>trends</div>;
 };
 
