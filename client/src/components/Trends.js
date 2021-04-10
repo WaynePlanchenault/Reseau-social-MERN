6efab4b5252bdf0f843d70a2459763d469ccf5e1
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-10 19:46:36 +0200

    useEffect composant trends

--- client/src/components/Trends.js
+++ client/src/components/Trends.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { isEmpty } from "./Utils";
 
 const Trends = () => {
   const posts = useSelector((state) => state.allPostsReducer);
   const usersData = useSelector((state) => state.usersReducer);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!isEmpty(posts[0])) {
+      const postsArr = Object.keys(posts).map((i) => posts[i]);
+      let sortedArray = postsArr.sort((a, b) => {
+        return b.likers.length - a.likers.length;
+        // permet de récupérer tous les posts puis de les maper, de les trier,
+        // puis de retourner le post qui a le plus de like
+      });
+      console.log(sortedArray);
+    }
+  }, [posts]);
 
   return <div>trends</div>;
 };
