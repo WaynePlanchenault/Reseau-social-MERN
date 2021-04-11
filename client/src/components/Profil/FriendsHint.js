b0c31601d0e8f5ca7c103a9bfb6ae4290e5edcb5
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-11 16:55:48 +0200

    useEffect suggestion abonnement

--- client/src/components/Profil/FriendsHint.js
+++ client/src/components/Profil/FriendsHint.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { isEmpty } from "../Utils";
 
 const FriendsHint = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -6,6 +8,16 @@ const FriendsHint = () => {
   const [friendsHint, setFriendsHint] = useState([]);
   const userData = useSelector((state) => state.userReducer);
   const usersData = useSelector((state) => state.usersReducer);
+
+  const notFriendList = () => {};
+
+  useEffect(() => {
+    if (playOnce && !isEmpty(usersData[0]) && !isEmpty(userData._id)) {
+      notFriendList();
+      setIsLoading(false);
+      setPlayOnce(false);
+    }
+  }, [usersData, usersData, playOnce]);
 
   return <div>edezde</div>;
 };
